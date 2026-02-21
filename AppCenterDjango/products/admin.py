ee536bb03503716a1b44c2309da7fb054a0140ae
avAs <abas.zade@outlook.com>
2026-02-21 14:12:03 +0330

    add expert admin show in admin page

--- AppCenterDjango/products/admin.py
+++ AppCenterDjango/products/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import product
 
+
 # Register your models here.
-admin.site.register(product)
+@admin.register(product)
+class ProductAdmin(admin.ModelAdmin):
+    ordering = ("name",)
+    sorted_fields = ("name", "price", "created_at", "updated_at")
+    list_display = ("name", "price", "created_at", "updated_at")
+    search_fields = ("name", "description")
+    list_filter = ("created_at", "updated_at")
